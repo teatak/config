b58f77dc5abe9c9d647e3e03d84070af6d7f1ee4
Yang <yangglive@icloud.com>
2022-06-08 17:13:40 +0800

    change mongo

--- sections/mongo.go
+++ sections/mongo.go
@@ -3,14 +3,8 @@ package sections
 import "github.com/teatak/config"
 
 type mongo struct {
-	Uri            string   `yaml:"uri,omitempty"`
-	Hosts          []string `yaml:"hosts,omitempty"`
-	Database       string   `yaml:"database,omitempty"`
-	ReplicaSetName string   `yaml:"replica_set_name,omitempty"`
-	Username       string   `yaml:"username,omitempty"`
-	Password       string   `yaml:"password,omitempty"`
-	Source         string   `yaml:"source,omitempty"`
-	PoolSize       int      `yaml:"pool_size,omitempty"`
+	Uri      string `yaml:"uri,omitempty"`
+	Database string `yaml:"database,omitempty"`
 }
 
 type mongoSection map[string]*mongo
