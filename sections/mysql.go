36d3ebd2b0293da6e96d66f82276d8a5efbc5e2c
Yang <yangglive@icloud.com>
2023-02-14 11:08:43 +0800

    add mysql section

---
+++ sections/mysql.go
@@ -0,0 +1,24 @@
+package sections
+
+import "github.com/teatak/config"
+
+type mysql struct {
+	Uri      string `yaml:"uri,omitempty"`
+	Database string `yaml:"database,omitempty"`
+}
+
+type mysqlSection map[string]*mysql
+
+func (s *mysqlSection) SectionName() string {
+	return "mysql"
+}
+
+func (s *mysqlSection) Default() *mysql {
+	return Mysql["default"]
+}
+
+var Mysql = mysqlSection{}
+
+func init() {
+	config.Load(&Mysql)
+}
