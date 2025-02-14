01d6d0fbbb92e377d089cb0db610a1ed1d2a65d5
Yang <yangglive@icloud.com>
2025-02-14 12:16:13 +0800

    change

--- sections/redis.go
+++ sections/redis.go
@@ -3,16 +3,19 @@ package sections
 import "github.com/teatak/config"
 
 type redis struct {
-	Sentinel *struct {
-		Master    string   `yaml:"master,omitempty"`
-		Addresses []string `yaml:"addresses,omitempty"`
-	} `yaml:"sentinel,omitempty"`
-	Cluster *struct {
-		Addresses []string `yaml:"addresses,omitempty"`
-	} `yaml:"cluster,omitempty"`
-	Address  string `yaml:"address,omitempty"`
-	Password string `yaml:"password,omitempty"`
-	Db       int    `yaml:"db,omitempty"`
+	// 单个主机或集群配置
+	// 例如：[]string{"192.168.1.10:6379"}
+	Addrs []string `yaml:"addrs,omitempty"`
+	// 哨兵 Master Name，仅适用于 `Failover Client`
+	MasterName string `yaml:"masterName,omitempty"`
+	// ClientName 和 `Options` 相同，会对每个Node节点的每个网络连接配置
+	ClientName string `yaml:"clientName,omitempty"`
+	// 设置 DB, 只针对 `Redis Client` 和 `Failover Client`
+	DB               int    `yaml:"db,omitempty"`
+	Username         string `yaml:"username,omitempty"`
+	Password         string `yaml:"password,omitempty"`
+	SentinelUsername string `yaml:"sentinelUsername,omitempty"`
+	SentinelPassword string `yaml:"sentinelPassword,omitempty"`
 }
 
 type redisSection map[string]*redis
