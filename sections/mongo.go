ddb0532116a035ba0bef7d6db89ae1ab1523d334
yangglive <yangglive@icloud.com>
2026-01-14 13:20:00 +0800

    update v2

--- sections/mongo.go
+++ sections/mongo.go
@@ -3,8 +3,10 @@ package sections
 import "github.com/teatak/config/v2"
 
 type mongo struct {
-	URI      string `yaml:"uri,omitempty"`
-	Database string `yaml:"database,omitempty"`
+	URI            string `yaml:"uri,omitempty"`
+	Database       string `yaml:"database,omitempty"`
+	ConnectTimeout uint64 `yaml:"connect_timeout,omitempty"` //second
+	MaxPoolSize    uint64 `yaml:"max_pool_size,omitempty"`   //100
 }
 
 type mongoSection map[string]*mongo
