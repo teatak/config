070a81aefa3687da5632fb413c0e7896a87aca51
yangglive <yangglive@icloud.com>
2026-01-24 00:20:05 +0800

    chore: update smtp username field

--- sections/smtp.go
+++ sections/smtp.go
@@ -5,7 +5,7 @@ import "github.com/teatak/config/v2"
 type smtp struct {
 	Address  string `yaml:"address,omitempty"`
 	Name     string `yaml:"name,omitempty"`
-	UserName string `yaml:"username,omitempty"`
+	Username string `yaml:"username,omitempty"`
 	Password string `yaml:"password,omitempty"`
 }
 
